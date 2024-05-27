fb1749e10ffd7cb8587ad590fe6c0bd2336246c6
Leila <leilaabdulahad@hotmail.com>
2024-05-27 11:11:22 +0200

    adds link to posts

--- app/event/allevents/page.jsx
+++ app/event/allevents/page.jsx
@@ -1,11 +1,12 @@
 'use client'
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 function AllEvents() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/posts')
+    fetch('http://localhost:3001/api/posts')
       .then(response => response.json())
       .then(data => setPosts(data))
       .catch(error => console.error('Error:', error));
@@ -15,17 +16,18 @@ function AllEvents() {
     <div>
       <h1>Posts</h1>
       {posts.map(post => (
-        <div key={post._id}>
-          <h2>{post.title}</h2>
-          <p>{post.location}</p>
-          {post.imageId && (
-            <img src={post.image} alt={post.title} width={500} height={300} />
-          )}
-          <p>{post.date}</p>
-          <p>{post.time}</p>
-          <p>{post.body}</p>
-        
-        </div>
+        <Link href={`/post/${post._id}`} key={post._id}>
+          
+            <h2>{post.title}</h2>
+            <p>{post.location}</p>
+            {post.imageId && (
+              <img src={post.image} alt={post.title} width={500} height={300} />
+            )}
+            <p>{post.date}</p>
+            <p>{post.time}</p>
+            <p>{post.body}</p>
+          
+        </Link>
       ))}
     </div>
   );
