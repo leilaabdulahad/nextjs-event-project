61e570e799215a64ff1fb0753c119394827cb4d9
Leila <leilaabdulahad@hotmail.com>
2024-05-27 09:27:05 +0200

    adds listing events comp.

--- app/event/allevents/page.jsx
+++ app/event/allevents/page.jsx
@@ -1,6 +1,34 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+
 function AllEvents() {
-    return (
-      <div>Allevents</div>
-    )
-  }
-  export default AllEvents
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.map(post => (
+        <div key={post._id}>
+          <h2>{post.title}</h2>
+          <p>{post.location}</p>
+          {post.imageId && (
+            <img src={post.image} alt={post.title} width={500} height={300} />
+          )}
+          <p>{post.date}</p>
+          <p>{post.time}</p>
+          <p>{post.body}</p>
+        
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AllEvents;
